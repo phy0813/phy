859a4d75ff28fce1ca2876b52b89bf4efcee51fc
phy0813 <2551277737@qq.com>
2020-10-20 21:35:56 +0800

    第一次向github提交代码

---
+++ phy01.py
@@ -0,0 +1,35 @@
+# print("hello world!")# 字符串
+# print (2333)# 整数
+# print (2.33)# 小数
+# print (True)# 布尔值
+# print (())#元组
+# print([])#数组
+# print({})#字典
+# """
+# 锄禾日当午
+# 汗滴禾下土
+# """
+# print("皮蛋",123)
+# print("嘻嘻"+"哈哈")# 字符串的拼接
+# print("嘻嘻哈哈"*3)# 字符串相乘
+# print(98*(15+16)-2/9)
+# print(1>2)
+# print(1<2)
+#  a = float(input("请输入："))
+#  b = float(input("请输入："))
+# print("input获取的内容:",a+b)
+# c = type(233)
+# print(c)
+# print(type("写写画画"))
+# print(type(233))
+# print(type(2.33))
+# print(type(False))
+# print(type(()))
+# print(type([]))
+# print(type({}))  
+a = (input("请输入："))
+b = int(input("请输入："))
+print("input获取的值:",a*b)
+c = a*b
+print("字数:",len(c))
+# ssdd
